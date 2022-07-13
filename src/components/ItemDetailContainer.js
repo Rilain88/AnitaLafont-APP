7393abdcd2d5791dfb2e1e5d9c4373146993da59
Rilain <garcia.agus.lucas@gmail.com>
2022-07-13 20:49:18 -0300

    Agregue actualizacion mencionada - pendiente README-Pendiente Categrias

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useParams } from 'react';
 import ItemDetail from './ItemDetail';
 
 
 function ItemDetailContainer() {
 
   const  [cardFetch, setCardFetch] = useState([])
+  const {shoesId} = useParams ()
  
-  useEffect((type, shoesId)=>{
+  useEffect(()=>{
     fetch('data.json') // trae la info del response comppleto
     .then((resp) => resp.json()) // Extrae la info a utilizar del Response
-    .then((data) => setCardFetch((data.find( type.id === shoesId)))) // Setea esa info para nuestro State
+    .then((data) => setCardFetch((data.find( type => type.id === shoesId)))) // Setea esa info para nuestro State
     .catch(err => console.log(err))
-  }, [])
+  }, [shoesId])
 
   return (
     <section className='cuerpo'>
